29e57bc1cf7787945a81653690ef85c28c0c2bfa
KartikaApDD <dharmadivyak@wsc.school.nz>
2024-05-02 12:45:27 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Ask the user their name and save it
+input("What is your name?")
+print("Gles")
+
+# Greet the user and introduce the quiz
+print("Welcome to the quiz, Gles")
+print("I hope you have a nice day today!")
+print("In this quiz you need to guess what my favourite food is")
+
+# Ask the user a question
+answer= input("Do you know what my favourite food is?")
+print("Bread")
+
+# Tell the the correct answer
+print("You are wrong! It is sushi")
+
+# End the quiz
+print("Oh no the quiz has ended! Sad to hear it but I am happy that you want to use mine.")
