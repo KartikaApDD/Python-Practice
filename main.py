4c5717ef223910f261813c89cef78c3e258557d9
KartikaApDD <dharmadivyak@wsc.school.nz>
2024-05-03 13:29:49 +1200

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# Ask the user their name and save it
-input("What is your name?")
-print("Gles")
+# Ask the user their name and save it 
+name = input("What's your name?")
 
 # Greet the user and introduce the quiz
 print("Welcome to the quiz, Gles")
@@ -8,11 +7,13 @@ print("I hope you have a nice day today!")
 print("In this quiz you need to guess what my favourite food is")
 
 # Ask the user a question
-answer= input("Do you know what my favourite food is?")
-print("Bread")
+answer = input("Do you know what my favourite food is?")
 
-# Tell the the correct answer
-print("You are wrong! It is sushi")
+# Check the user's answer and give feedback
+if answer == "sushi":
+    print("Yey, you got it right!")
+else:
+    print("You don't get it right. It's sushi")
 
 # End the quiz
 print("Oh no the quiz has ended! Sad to hear it but I am happy that you want to use mine.")
