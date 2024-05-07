8ec5717e96c30b95da8aeafd0ae0f492b88d7656
KartikaApDD <dharmadivyak@wsc.school.nz>
2024-05-07 12:03:54 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+score = 0
 # Ask the user their name and save it 
 name = input("What's your name?")
 
@@ -12,8 +13,11 @@ answer = input("Do you know what my favourite food is?")
 # Check the user's answer and give feedback
 if answer == "sushi":
     print("Yey, you got it right!")
+    score+= 20
+elif answer == "":
+    print("Oh you didn't know that? My favourite food is sushi")
 else:
     print("You don't get it right. It's sushi")
-
+    score+= 7
 # End the quiz
-print("Oh no the quiz has ended! Sad to hear it but I am happy that you want to use mine.")
+print("Well done! You have finished your quiz, you got", score, "points")
